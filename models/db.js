907887bec00f34b796986e5fcc8b2d299d1e5414
HebutJipeng <282602893@qq.com>
2016-08-25 20:25:59 +0800

    这次的session用着用问题需要解决

--- models/db.js
+++ models/db.js
@@ -3,4 +3,4 @@ var settings = require('../settings'),
 	Db = mongodb.Db,
 	Connection = mongodb.Connection,
 	Server = require('mongodb').Server;
-module.exports = new Db(settings.db, new Server(settings.hostm settings.port), {safe: true});
+module.exports = new Db(settings.db, new Server(settings.host, settings.port), {safe: true});
--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,6 @@
+var crypto = require('crypto'),
+	User = require('../models/user.js');
+
 module.exports = function(app) {
 	app.get('/', function(req, res){
 		res.render('index', { title: 'Express'});
@@ -6,6 +9,46 @@ module.exports = function(app) {
 		res.render('reg', {title: '注册'});
 	});
 	app.post('/reg', function(req, res){
+		var name = req.body.name,
+			password = req.body.password,
+			password_re = req.body['password-repeat'];
+		//检测两次密码是否一致
+		if (password_re != password) {
+			req.flash('error', '两次输入的密码不一致！');
+			return res.redirect('/reg'); //返回注册页
+		}
+		//生成密码的md5
+		var md5 = crypto.createHash('md5'),
+			password = md5.update(req.body.password).digest('hex');
+		var newUser = new User({
+			name: name,
+			password: password,
+			email: req.body.email
+		});
+		//检测用户名是否已经存在
+		User.get(newUser.name, function(err, user) {
+			if (err) {
+				req.flash('error', err);
+				return res.redirect('/');
+			}
+			if (user) {
+				req.flash('error', '用户已存在！')
+				return res.redirect('/reg'); //返回注册页
+			}
+
+			//如果不存在则新增用户
+			newUser.save(function(err, user) {
+				if (err) {
+					req.flash('error', err);
+					res.redirect('/reg'); //注册失败
+				}
+
+				req.session.user = newUser;//用户信息
+				req.flash('success', '注册成功');
+				res.redirect('/'); //注册成功后返回主页
+
+			});
+		});
 
 	});
 	app.get('/login', function(req, res){
